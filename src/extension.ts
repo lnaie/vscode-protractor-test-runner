70ef5d0ecb59f779b850c8977a5a3275325ff8fe
Lucian Naie <Lucian Naie>
2016-11-29 23:16:26 +0100

    Simple code, keep it simple. :)

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as runner from './commands/protractor-runner';
+var spawnCMD = require('spawn-command');
+var treeKill = require('tree-kill');
+var process = null;
+var commandOutput: vscode.OutputChannel = null;
 
 // This method is called when the extension is activated.
 // The extension is activated the very first time the command is executed.
@@ -23,7 +26,8 @@ export function activate(context: vscode.ExtensionContext) {
 
 			filePath = vscode.window.activeTextEditor.document.uri.fsPath;
 		}
-        runner.execute(filePath, commandOutput);
+
+        startProcess(filePath);
     });
     context.subscriptions.push(protractorTestRunnerDisposable);
 
@@ -32,9 +36,82 @@ export function activate(context: vscode.ExtensionContext) {
         commandOutput.show();
     })
     context.subscriptions.push(protractorTestRunnerLogDisposable);
-}
+};
 
 // This method is called when the extension is deactivated
 export function deactivate() {
-    runner.killActiveProcess();
-}
+    killActiveProcess();
+};
+
+// Tries to run one command at a time.
+function startProcess(filePath: string) {
+    // Already running one?
+    if (process) {
+        const msg = 'There is a command running right now. Terminate it before executing a new command?';
+        vscode.window
+            .showWarningMessage(msg, 'Ok', 'Cancel')
+            .then((choice) => {
+                if (choice === 'Ok') {
+                    killActiveProcess();
+                }
+            });
+        return;
+    }
+
+    // Show log window
+    commandOutput.show();
+
+    // Start a new command
+    var cmd = `protractor --specs ${filePath}`;
+    commandOutput.appendLine(`> Running command: ${cmd}...`);
+
+    runShellCommand(cmd, vscode.workspace.rootPath)
+        .then(() => {
+            commandOutput.appendLine(`> Command finished successfully.`);
+        })
+        .catch((reason) => {
+            commandOutput.appendLine(`> ERROR: ${reason}`);
+        });
+};
+
+// Tries to kill the active process that is running a command.
+function killActiveProcess() {
+    if (!process)
+        return;
+
+    commandOutput.appendLine(`> Killing PID ${process.pid}...`);
+    treeKill(process.pid, 'SIGTERM', (err) => {
+        if (err) {
+            commandOutput.appendLine("> ERROR: Failed to kill process.");
+        }
+        else {
+            commandOutput.appendLine("> Process killed.");
+            process = null;
+        }
+    });
+};
+
+function printOutputDelegate(data) { 
+    commandOutput.append(data.toString());
+};
+
+function runShellCommand(cmd:string, cwd:string) {
+    return new Promise((accept, reject) => {
+        var opts : any = {};
+        if (vscode.workspace) {
+            opts.cwd = cwd;
+        }
+
+        process = spawnCMD(cmd, opts);
+        process.stdout.on('data', printOutputDelegate);
+        process.stderr.on('data', printOutputDelegate);
+        process.on('close', (status) => {
+            if (status) {
+                reject(`Command exited with status code ${status}.`);
+            } else {
+                accept();
+            }
+            process = null;
+        });
+    });
+};
